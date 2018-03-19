2cfd47aafa8024fb53fc1824c81df4763093018c
Shrikant Patnaik <shrikant.patnaik@gmail.com>
2018-03-19 17:13:57 +0800

    Add delay to vpn server creation

--- vpn/create_server.sh
+++ vpn/create_server.sh
@@ -2,4 +2,5 @@
 docker-compose run --rm openvpn ovpn_genconfig -u udp://$1
 docker-compose run --rm openvpn ovpn_initpki
 docker-compose up -d openvpn
+sleep 1m # Wait for openvpn to start
 docker-compose up -d openvpn_exporter
